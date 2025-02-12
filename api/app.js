eff9c2e6ef10803cf5ec031cc5e777d49194a12f
Siva V <sivavijayakumar1222@gmail.com>
2025-02-12 08:39:19 +0530

    updated

--- api/app.js
+++ api/app.js
@@ -11,7 +11,8 @@ import testRoute from './routes/test.route.js';
 import userRoute from './routes/user.route.js'
 import chatRoute from './routes/chat.route.js'
 import messageRoute from './routes/message.route.js';
-import { initializeSocket } from "./app.js"; 
+// import { initializeSocket } from "./app.js"; 
+import { initializeSocket } from './socket/app.js';
 // import { env } from 'process';
 
 dotenv.config()
